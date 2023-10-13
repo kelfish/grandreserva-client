99cdfabd303c3c0b6f1e8ba224463f0065160bb9
Kel Phillipson <kel@kelfish.com>
2023-10-12 22:18:09 -0700

    Fix issue with lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,7 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  parserOptions: {
-    project: true,
-    tsconfigRootDir: __dirname,
-  },
   root: true,
 };
